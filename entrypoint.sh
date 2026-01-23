0efdfde0d86bf1b52aef4b4273bb501d06826e0d
mtf-aber-ac-uk <mtf@aber.ac.uk>
2026-01-23 14:41:55 +0000

    Update permissions for config.php
    Ensure the config file is writable by the web server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,9 @@ service cron start
 # Ensure daily cron jobs are executable
 chmod +x /etc/cron.daily/*
 
+# Make sure the config file can be written to
+chown www-data /var/www/html/include/config.php
+chmod 744 /var/www/html/include/config.php
+
 # Start Apache in the foreground (keeps the container alive)
 apachectl -D FOREGROUND
